3e837c9c3bec2d460fbd3076d902fd18cd99d10e
John Kim <joki9448@gmail.com>
2022-08-11 21:55:24 -0400

    rusty lol

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,6 @@
 class TasksController < ApplicationController
+    
+    def index
+        render json: Task.all, status: 200
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,10 @@
 class UsersController < ApplicationController
-    
+
+    def index 
+        render json: User.all, status: 200
+    end
+
+    # def create
+    #     user = User.create() 
+    # end
 end
