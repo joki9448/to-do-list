6251dac955ceb666fe9ceb02cbf35729e6ee6519
John Kim <joki9448@gmail.com>
2022-08-16 20:13:44 -0400

    set up cookies

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ Bundler.require(*Rails.groups)
 module ToDoList
   class Application < Rails::Application
     config.load_defaults 7.0
-
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+    config.action_dispatch.cookies_same_site_protection = :strict
   end
 end
